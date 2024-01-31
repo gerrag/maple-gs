2d392ad648cbc2d2329d6d0b477cc952e8913b0d
gerrag <gerrag@myumanitoba.ca>
2024-01-30 22:23:05 -0600

    Create Page components

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Tabs, Tab, Navbar, Container } from 'react-bootstrap';
+import HomePage from "./components/HomePage";
+import PastTestPage from "./components/PastTestPage";
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <p>Welcome to your Electron application.</p>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>G08 MAPLE Ground Station</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Tabs
+        defaultActiveKey="home"
+        id="gsTabs"
+        className="mb-3"
+        justify
+      >
+        <Tab eventKey="home" title="Home">
+          <HomePage />
+        </Tab>
+        <Tab eventKey="pastTest" title="pastTest">
+          <PastTestPage />
+        </Tab>
+      </Tabs>
     </div>
   );
 }
