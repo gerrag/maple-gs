d02a606129a81e723521f7fe2eece0681c2618cf
gerrag <gerrag@myumanitoba.ca>
2024-02-29 21:38:16 -0600

    Adjust GUI size for smaller screens

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1600,
-    height: 1200,
+    height: 1000,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
       nodeIntegration: true, // Enable Node.js integration
